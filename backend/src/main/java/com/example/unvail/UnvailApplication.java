dac5f5009f1d6e90b8bd2a0fa0a38fe9ed9cc222
name <email>
2026-02-08 17:50:13 +0900

    Docker commit total

---
+++ backend/src/main/java/com/example/unvail/UnvailApplication.java
@@ -0,0 +1,13 @@
+package com.example.unvail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnvailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnvailApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/unvail/UnvailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unvail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnvailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
